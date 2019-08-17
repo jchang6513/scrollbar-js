436e1cbd07696f60fd52ef972245516c0c9089f4
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2019-08-17 23:05:30 +0800

    bind with elements and add scrolling class

--- src/scrollable.js
+++ src/scrollable.js
@@ -4,28 +4,34 @@ var scrollableKeys = Object.keys(scrollables);
 for (var i = 0; i < scrollableKeys.length; i++) {
   var scrollable = scrollables[scrollableKeys[i]];
   var frame = scrollable.getElementsByClassName("scroll-frame")[0];
+  var content = scrollable.getElementsByClassName("scroll-content")[0];
+  var scrollBarTimeout;
+  var scrollBar = document.createElement("div");
+  scrollBar.className = "scroll-bar";
+  scrollable.append(scrollBar);
+
   frame.addEventListener(
     "scroll",
-    function(e, scrollable) {
-      console.log(scrollable);
-      var frame = this.getElementsByClassName("scroll-frame")[0];
-      var content = this.getElementsByClassName("scroll-content")[0];
-      var frameRect = frame.getBoundingClientRect();
-      var contentRect = content.getBoundingClientRect();
+    function(e) {
+      var frameRect = this.frame.getBoundingClientRect();
+      var contentRect = this.content.getBoundingClientRect();
       var scrollDelta = frameRect.top - contentRect.top;
       var barRatio = (frameRect.height / contentRect.height) * 100;
-      console.log(scrollDelta, barRatio);
-      var scrollBar = this.getElementsByClassName("scroll-bar")[0];
-      var barExist = !!scrollBar;
-      if (!barExist) {
-        scrollBar = document.createElement("div");
-        scrollBar.className = "scroll-bar";
+
+      this.scrollBar.style.top = scrollDelta + "px";
+      this.scrollBar.style.height = barRatio + "%";
+
+      if (this.scrollBar.className === "scroll-bar scrolling") {
+        clearTimeout(this.scrollBarTimeout);
+      } else {
+        this.scrollBar.className = "scroll-bar scrolling";
       }
-      scrollBar.style.top = scrollDelta + "px";
-      scrollBar.style.height = barRatio + "%";
-      if (!barExist) {
-        this.append(scrollBar);
-      }
-    }.bind(scrollable)
+      this.scrollBarTimeout = setTimeout(
+        function() {
+          this.className = "scroll-bar";
+        }.bind(this.scrollBar),
+        1500
+      );
+    }.bind({ scrollable, frame, content, scrollBarTimeout, scrollBar })
   );
 }
