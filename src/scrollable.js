5a918309aeb0faf2c7450482f00b199134afd224
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2019-08-18 03:14:24 +0800

    add horizontal scrollbar

--- src/scrollable.js
+++ src/scrollable.js
@@ -5,10 +5,14 @@ for (var i = 0; i < scrollableKeys.length; i++) {
   var scrollable = scrollables[scrollableKeys[i]];
   var frame = scrollable.getElementsByClassName("scroll-frame")[0];
   var content = scrollable.getElementsByClassName("scroll-content")[0];
-  var scrollBarTimeout;
-  var scrollBar = document.createElement("div");
-  scrollBar.className = "scroll-bar";
-  scrollable.append(scrollBar);
+  var scrollbarX = createClassElement("div", "scrollbar scrollbar-x");
+  scrollbarX.style.left = "0px";
+  var scrollbarY = createClassElement("div", "scrollbar scrollbar-y");
+  scrollbarY.style.top = "0px";
+  scrollable.append(scrollbarX);
+  scrollable.append(scrollbarY);
+  var scrollBarTimeoutX;
+  var scrollBarTimeoutY = 1;
 
   frame.addEventListener(
     "scroll",
@@ -17,41 +21,100 @@ for (var i = 0; i < scrollableKeys.length; i++) {
         frame: this.frame,
         content: this.content
       });
-      updateScrollbar.call(this.scrollBar, state.offSet, state.length);
-      setScrollbarTimeout.call(
-        {
-          scrollBar: this.scrollBar,
-          scrollBarTimeout: this.scrollBarTimeout
-        },
-        1500
-      );
-    }.bind({ scrollable, frame, content, scrollBarTimeout, scrollBar })
+
+      if (state.length.x < 100) {
+        updateScrollbarX.call(
+          { scrollbar: this.scrollbarX },
+          state.offSet.x,
+          state.length.x
+        );
+        this.scrollBarTimeoutX = setScrollbarTimeout.call(
+          {
+            scrollbar: this.scrollbarX,
+            scrollBarTimeout: this.scrollBarTimeoutX
+          },
+          3000
+        );
+      }
+
+      if (state.length.y < 100) {
+        updateScrollbar.call(
+          { scrollbar: this.scrollbarY },
+          state.offSet.y,
+          state.length.y
+        );
+        this.scrollBarTimeoutY = setScrollbarTimeout.call(
+          {
+            scrollbar: this.scrollbarY,
+            scrollBarTimeout: this.scrollBarTimeoutY
+          },
+          3000
+        );
+      }
+    }.bind({
+      scrollable,
+      frame,
+      content,
+      scrollbarX,
+      scrollbarY,
+      scrollBarTimeoutX,
+      scrollBarTimeoutY
+    })
   );
+}
+
+function createClassElement(tag, className) {
+  var el = document.createElement(tag);
+  el.className = className;
+  return el;
 }
 
 function getScrollbarState() {
   var frameRect = this.frame.getBoundingClientRect();
   var contentRect = this.content.getBoundingClientRect();
-  var barRatio = frameRect.height / contentRect.height;
-  var scrollDelta = (frameRect.top - contentRect.top) * barRatio;
-  return { offSet: scrollDelta, length: barRatio * 100 };
+  var lengthRatio = {
+    x: frameRect.width / contentRect.width,
+    y: frameRect.height / contentRect.height
+  };
+  var offSet = {
+    x: Math.round((frameRect.left - contentRect.left) * lengthRatio.x),
+    y: Math.round((frameRect.top - contentRect.top) * lengthRatio.y)
+  };
+  var length = {
+    x: Math.round(lengthRatio.x * 100),
+    y: Math.round(lengthRatio.y * 100)
+  };
+  return { offSet, length };
+}
+
+function updateScrollbarX(offSet, length) {
+  if (parseInt(this.scrollbar.style.left, 10) !== offSet) {
+    this.scrollbar.style.left = offSet + "px";
+    this.scrollbar.classList.add("scrolling");
+  }
+  if (this.scrollbar.style.width !== length + "%") {
+    this.scrollbar.style.width = length + "%";
+  }
 }
 
 function updateScrollbar(offSet, length) {
-  this.style.top = offSet + "px";
-  this.style.height = length + "%";
+  if (parseInt(this.scrollbar.style.top, 10) !== offSet) {
+    this.scrollbar.style.top = offSet + "px";
+    this.scrollbar.classList.add("scrolling");
+  }
+  if (this.scrollbar.style.height !== length + "%") {
+    this.scrollbar.style.height = length + "%";
+  }
 }
 
 function setScrollbarTimeout(timeout) {
-  if (this.scrollBar.className === "scroll-bar scrolling") {
+  if (this.scrollbar.classList.contains("scrolling")) {
     clearTimeout(this.scrollBarTimeout);
-  } else {
-    this.scrollBar.classList.add("scrolling");
   }
-  this.scrollBarTimeout = setTimeout(
+  return setTimeout(
     function() {
       this.classList.remove("scrolling");
-    }.bind(this.scrollBar),
+    }.bind(this.scrollbar),
     timeout
   );
 }
