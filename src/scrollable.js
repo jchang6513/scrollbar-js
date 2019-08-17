9ce0c19d020875197104435d8a9a61ad071dec16
Yi-Wei Chang <31400914+jchang6513@users.noreply.github.com>
2019-08-17 23:59:46 +0800

    extract scroll state function

--- src/scrollable.js
+++ src/scrollable.js
@@ -13,18 +13,28 @@ for (var i = 0; i < scrollableKeys.length; i++) {
   frame.addEventListener(
     "scroll",
     function(e) {
-      var frameRect = this.frame.getBoundingClientRect();
-      var contentRect = this.content.getBoundingClientRect();
-      var barRatio = frameRect.height / contentRect.height;
-      var scrollDelta = (frameRect.top - contentRect.top) * barRatio;
-
-      updateScrollbar.bind(this.scrollBar)(scrollDelta, barRatio * 100);
-      setScrollBarTimeout.bind({
-        scrollBar: this.scrollBar,
-        scrollBarTimeout: this.scrollBarTimeout
-      })(1500);
+      var state = getScrollbarState.call({
+        frame: this.frame,
+        content: this.content
+      });
+      updateScrollbar.call(this.scrollBar, state.offSet, state.length);
+      setScrollbarTimeout.call(
+        {
+          scrollBar: this.scrollBar,
+          scrollBarTimeout: this.scrollBarTimeout
+        },
+        1500
+      );
     }.bind({ scrollable, frame, content, scrollBarTimeout, scrollBar })
   );
+}
+
+function getScrollbarState() {
+  var frameRect = this.frame.getBoundingClientRect();
+  var contentRect = this.content.getBoundingClientRect();
+  var barRatio = frameRect.height / contentRect.height;
+  var scrollDelta = (frameRect.top - contentRect.top) * barRatio;
+  return { offSet: scrollDelta, length: barRatio * 100 };
 }
 
 function updateScrollbar(offSet, length) {
@@ -32,7 +42,7 @@ function updateScrollbar(offSet, length) {
   this.style.height = length + "%";
 }
 
-function setScrollBarTimeout(timeout) {
+function setScrollbarTimeout(timeout) {
   if (this.scrollBar.className === "scroll-bar scrolling") {
     clearTimeout(this.scrollBarTimeout);
   } else {
